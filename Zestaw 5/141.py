aa276f7d346d0af34cf01bd8046f8f805578c622
Mserafin <Marcin.serafin0325@gmail.com>
2024-11-28 09:40:04 +0100

    132 140 141

---
+++ Zestaw 5/141.py
@@ -0,0 +1,11 @@
+def zad141(T,mase):
+    def rek(T,mase,i):
+        if mase==0:
+            return True
+        if i>=len(T):
+            return False
+        return rek(T,mase,i+1) or rek(T,mase-T[i],i+1) or rek(T,mase+T[i],i+1)
+    return rek(T,mase,0)
+
+T=[1,3,7,8]
+print(zad141(T,5))
